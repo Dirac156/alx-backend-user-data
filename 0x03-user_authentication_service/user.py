5a064bd2be29b65ce508e37d983dadd09f9d9ad0
= <=>
2021-09-01 19:46:59 +0200

    use the sqlalchemy false instead of python false

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """ User base module """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.expression import false
 
 
 Base = declarative_base()
@@ -13,7 +13,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    email = Column(String, nullable=false)
+    hashed_password = Column(String, nullable=false)
     session_id = Column(String, nullable=True)
     reset_token = Column(String, nullable=True)
