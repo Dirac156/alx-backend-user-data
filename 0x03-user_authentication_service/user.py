e6bf1af8b1c4e9ca62f5a1f7fd45a1f3072f7c99
= <=>
2021-09-01 19:36:53 +0200

    implemented user cbase classe

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" User base module """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import null
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ The base model for users """
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
