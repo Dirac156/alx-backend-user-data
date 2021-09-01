1b9c74d2b09d679c8a6a8227ccfed77ca4ad9ee9
= <=>
2021-09-01 19:49:48 +0200

    add the number of character require for each attribute

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,6 @@
 """ User base module """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.expression import false
 
 
 Base = declarative_base()
@@ -13,7 +12,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=false)
-    hashed_password = Column(String, nullable=false)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=false)
+    hashed_password = Column(String(250), nullable=false)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
