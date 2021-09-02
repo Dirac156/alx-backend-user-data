629173c2ae22669945d25339c2acd60440b32d4d
= <=>
2021-09-02 23:39:10 +0200

    created a post method to register new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
 """ App """
-from flask import Flask
-from flask import jsonify
+import flask
+from auth import Auth
 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def hello_world() -> str:
     """ return welcome message """
     msg = {"message": "Bienvenue"}
-    return jsonify(msg)
+    return flask.jsonify(msg)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """ Add a new user to the db """
+    try:
+        email = flask.request.form['email']
+        password = flask.request.form['password']
+    except KeyError:
+        flask.abort(400)
+
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return flask.jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return flask.jsonify(msg)
 
 
 if __name__ == "__main__":
