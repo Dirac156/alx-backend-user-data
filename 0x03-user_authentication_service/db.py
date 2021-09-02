0084f9a003a83fa252c50bbb4e532c1f2599fa10
= <=>
2021-09-02 22:23:59 +0200

    recoreded user data to a session

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,4 +32,6 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ create a new user and save the credential into the db """
         user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
         return user
